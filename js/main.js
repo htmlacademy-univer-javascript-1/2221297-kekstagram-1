027fb37e5e72b2a554f67cc22bf69760b70ef695
Vikross <Viktoria.Emelkina@urfi.me>
2022-09-19 02:52:16 +0500

    3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-function getRandom(min, max) {
-  return Math.random() * (max - min + 1) + min;
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-console.log(Math.random(1, 100));
