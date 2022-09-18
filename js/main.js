beec8d5bdf567bc30b6561d12c27e1323d92a81f
Vikross <Viktoria.Emelkina@urfi.me>
2022-09-19 03:42:31 +0500

    second

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getRandom(min, max) {
+  return Math.random() * (max - min + 1)  + min;
 }
+//Источник:https://schoolsw3.com/js/js_random.php
